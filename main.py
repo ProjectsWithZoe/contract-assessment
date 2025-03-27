a6549b25e64abdf44383a91f960866fe779b87d5
Zoe Mwangi <your-email@example.com>
2025-03-27 22:48:08 +0000

    function to open pdf file

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import pdfplumber
+
+#open pdf file that needs assessing
+def openFile(pdf_file):
+    text = []
+    with pdfplumber.open(pdf_file) as pdf:
+        for page in pdf.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text.append(page_text)
+    return '\n'.join(text)
+
+pdf_text = openFile('CTAC.pdf')
+print(pdf_text)
